6a5de680c40b8828a0d212b964ccac325e7611b2
DESKTOP-PRG6F3N\dienn <diennd20102002@gmail.com>
2024-04-25 20:48:46 +0700

    update

--- Areas/Admin/Controllers/ReadersController.cs
+++ Areas/Admin/Controllers/ReadersController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using QLTV.Models;
+using X.PagedList;
 
 namespace QLTV.Areas.Admin.Controllers
 {
@@ -15,9 +18,15 @@ namespace QLTV.Areas.Admin.Controllers
         private PROJECT_QLTVContext context;
         public ReadersController(PROJECT_QLTVContext context) => this.context = context;
 
-        public IActionResult Index()
+        public IActionResult Index(string Search, int page = 1)
         {
-            return View();
+            int limit = 5;
+            var list = context.Readers.ToList();
+            if (!string.IsNullOrEmpty(Search))
+            {
+                list = list.Where(x => x.NameReader.Contains(Search) || x.PhoneReader.Contains(Search) || x.EmailReader.Contains(Search) || x.Passport.Contains(Search)).ToList();
+            }
+            return View(list.OrderByDescending(x => x.CreateDate).ToPagedList(page, limit));
         }
 
         public IActionResult Create()
@@ -41,10 +50,29 @@ namespace QLTV.Areas.Admin.Controllers
                 ViewBag.Phone = "Số điện thoại độc giả không được để trống";
                 check = false;
             }
-            var checkName = dataCheck.FirstOrDefault(p => p.PhoneReader.Equals(model.PhoneReader));
-            if (checkName != null)
+            if (!string.IsNullOrEmpty(model.PhoneReader))
             {
-                ViewBag.CheckPhone = "Số điện thoại độc giả đã tồn tại";
+                var checkName = dataCheck.FirstOrDefault(p => p.PhoneReader.Equals(model.PhoneReader));
+                if (checkName != null)
+                {
+                    ViewBag.CheckPhone = "Số điện thoại độc giả đã tồn tại";
+                    check = false;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(model.EmailReader))
+            {
+                var checkEmail = dataCheck.FirstOrDefault(x => x.EmailReader.Equals(model.EmailReader));
+                if (checkEmail != null)
+                {
+                    ViewBag.CheckEmail = "Địa chỉ email đã tồn tại";
+                    check = false;
+                }
+            }
+
+            if (string.IsNullOrEmpty(model.Passport))
+            {
+                ViewBag.Passport = "Số CMT, CCCD không được để trống";
                 check = false;
             }
 
@@ -53,9 +81,28 @@ namespace QLTV.Areas.Admin.Controllers
                 var checkPassport = dataCheck.FirstOrDefault(x => x.Passport.Equals(model.Passport));
                 if(checkPassport != null)
                 {
-                    ViewBag.Passport = "Số CCCD/CMT đã được sử dụng";
+                    ViewBag.CheckPassport = "Số CCCD/CMT đã được sử dụng";
+                    check = false;
                 }
+            }
+
+            if (model.DateOfBirth == null)
+            {
+                ViewBag.DateOfBirth = "Ngày sinh không được để trống";
                 check = false;
+            }
+
+            var files = HttpContext.Request.Form.Files;
+            if (files.Count() > 0 && files[0].Length > 0)
+            {
+                var file = files[0];
+                var FileName = file.FileName;
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Avatars", FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                    model.Avatar = FileName; // gán tên ảnh cho thuộc tinh Image
+                }
             }
 
             if (check)
@@ -67,6 +114,96 @@ namespace QLTV.Areas.Admin.Controllers
             }
             else
             {
+                return View();
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Readers data = context.Readers.FirstOrDefault(x => x.IdReader == id);
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Readers model)
+        {
+            Boolean check = true;
+            var dataCheck = context.Readers.ToList();
+            if (string.IsNullOrEmpty(model.NameReader))
+            {
+                ViewBag.Name = "Tên độc giả không được để trống";
+                check = false;
+            }
+
+            if (string.IsNullOrEmpty(model.PhoneReader))
+            {
+                ViewBag.Phone = "Số điện thoại độc giả không được để trống";
+                check = false;
+            }
+            var checkName = dataCheck.FirstOrDefault(p => p.PhoneReader.Equals(model.PhoneReader));
+            if (checkName != null)
+            {
+                ViewBag.CheckPhone = "Số điện thoại độc giả đã tồn tại";
+                check = false;
+            }
+
+            if (!string.IsNullOrEmpty(model.EmailReader))
+            {
+                var checkEmail = dataCheck.FirstOrDefault(x => x.EmailReader.Equals(model.EmailReader));
+                if (checkEmail != null)
+                {
+                    ViewBag.CheckEmail = "Địa chỉ email đã tồn tại";
+                }
+                check = false;
+            }
+
+            if (string.IsNullOrEmpty(model.Passport))
+            {
+                ViewBag.Passport = "Số CMT, CCCD không được để trống";
+                check = false;
+            }
+
+            if (!string.IsNullOrEmpty(model.Passport))
+            {
+                var checkPassport = dataCheck.FirstOrDefault(x => x.Passport.Equals(model.Passport));
+                if (checkPassport != null)
+                {
+                    ViewBag.CheckPassport = "Số CCCD/CMT đã được sử dụng";
+                }
+                check = false;
+            }
+
+            if (model.DateOfBirth != null)
+            {
+                ViewBag.DateOfBirth = "Ngày sinh không được để trống";
+                check = false;
+            }
+
+            var files = HttpContext.Request.Form.Files;
+            if (files.Count() > 0 && files[0].Length > 0)
+            {
+                var file = files[0];
+                var FileName = file.FileName;
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Avatars", FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                    model.Avatar = FileName; // gán tên ảnh cho thuộc tinh Image
+                }
+            }
+
+            if (check)
+            {
+                context.Readers.Update(model);
+                context.SaveChanges();
+                TempData["success"] = "Cập nhật thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Readers data = context.Readers.FirstOrDefault(x => x.IdReader == model.IdReader);
+
                 return View();
             }
         }
